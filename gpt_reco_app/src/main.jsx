b1b79d41340cb95b0df164a062f5dd17293ec709
Louis Paulet <louispaulet13@gmail.com>
2025-05-31 20:43:22 +0200

    fix weird margins when watching from smarphone

--- gpt_reco_app/src/main.jsx
+++ gpt_reco_app/src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
-      <div className="min-h-screen bg-gradient-to-br from-indigo-50 via-white to-indigo-50">
+      <div className="min-h-screen bg-gradient-to-br from-indigo-50 via-white to-indigo-50 overflow-x-hidden">
         <App />
       </div>
     </HashRouter>
