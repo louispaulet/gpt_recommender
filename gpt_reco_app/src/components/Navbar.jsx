455f522ed85c183aa4cae252105d69f60462f2d0
Louis Paulet <louispaulet13@gmail.com>
2025-05-31 21:12:19 +0200

    add home label on the navbar home button

--- gpt_reco_app/src/components/Navbar.jsx
+++ gpt_reco_app/src/components/Navbar.jsx
@@ -6,8 +6,9 @@ function Navbar() {
     <nav className="bg-gray-800 p-4 text-white">
       <ul className="flex space-x-4 max-w-4xl mx-auto">
         <li>
-          <Link to="/" className="hover:underline" aria-label="Home">
-            🏠
+          <Link to="/" className="flex items-center space-x-1" aria-label="Home">
+            <span role="img" aria-label="home" className="no-underline">🏠</span>
+            <span className="hover:underline">Home</span>
           </Link>
         </li>
         <li>
