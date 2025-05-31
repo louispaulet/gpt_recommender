619b3a3f9cf24458c6f67e38f2a1cd7d8faf9b0f
Louis Paulet <louispaulet13@gmail.com>
2025-05-31 15:17:46 +0200

    update vite config

--- gpt_reco_app/vite.config.js
+++ gpt_reco_app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/gpt_reco_app/',
+  base: '/gpt_recommender/',
   plugins: [react()],
 })
