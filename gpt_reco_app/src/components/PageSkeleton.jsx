e67aad4c65d42aaf563fee81f771ab70591f0af5
the_french_artist <louispaulet@users.noreply.github.com>
2025-06-09 19:15:55 +0200

    [gpt_reco_app] replace spinner fallback with page skeleton

---
+++ gpt_reco_app/src/components/PageSkeleton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function PageSkeleton() {
+  return (
+    <div className="py-12 px-4 sm:px-6 lg:px-8 max-w-5xl mx-auto animate-pulse space-y-6">
+      <div className="h-10 bg-gray-200 rounded w-3/4" />
+      <div className="space-y-3">
+        <div className="h-4 bg-gray-200 rounded" />
+        <div className="h-4 bg-gray-200 rounded w-5/6" />
+        <div className="h-4 bg-gray-200 rounded w-2/3" />
+      </div>
+      <div className="h-64 bg-gray-200 rounded" />
+    </div>
+  );
+}
+
+export default PageSkeleton;
