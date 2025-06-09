e69e3d72a2411e1d511e74f5ff4ac39c246ca07f
the_french_artist <louispaulet@users.noreply.github.com>
2025-06-09 19:24:49 +0200

    expand skeleton layout for loading

--- gpt_reco_app/src/components/PageSkeleton.jsx
+++ gpt_reco_app/src/components/PageSkeleton.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 function PageSkeleton() {
   return (
-    <div className="py-12 px-4 sm:px-6 lg:px-8 max-w-5xl mx-auto animate-pulse space-y-6">
-      <div className="h-10 bg-gray-200 rounded w-3/4" />
-      <div className="space-y-3">
-        <div className="h-4 bg-gray-200 rounded" />
-        <div className="h-4 bg-gray-200 rounded w-5/6" />
-        <div className="h-4 bg-gray-200 rounded w-2/3" />
+    <div className="py-12 px-4 sm:px-6 lg:px-8 max-w-5xl mx-auto space-y-6 min-h-[60vh] bg-gray-100">
+      <div className="animate-pulse space-y-6">
+        <div className="h-10 bg-gray-200 rounded w-3/4" />
+        <div className="space-y-3">
+          <div className="h-4 bg-gray-200 rounded" />
+          <div className="h-4 bg-gray-200 rounded w-5/6" />
+          <div className="h-4 bg-gray-200 rounded w-2/3" />
+        </div>
+        <div className="h-64 bg-gray-200 rounded" />
       </div>
-      <div className="h-64 bg-gray-200 rounded" />
     </div>
   );
 }
